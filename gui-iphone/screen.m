6d1363ca4037348107a4f44187b934a3c1570554
Andr? G?nther <Andre.G@gmx.de>
2010-10-14 23:30:51 -0500

    iphone branch v1

---
+++ gui-iphone/screen.m
@@ -0,0 +1,626 @@
+#undef Point
+#define Point _Point
+#undef Rect
+#define Rect _Rect
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <Foundation/NSRunLoop.h>
+
+#undef Rect
+#undef Point
+
+#undef nil
+
+#include "u.h"
+#include "lib.h"
+#include "kern/dat.h"
+#include "kern/fns.h"
+#include "error.h"
+#include "user.h"
+#include <draw.h>
+#include <memdraw.h>
+#include "screen.h"
+#include "keyboard.h"
+
+#define UNIMPL {printf("UNIMPL CALL: %s\n",__PRETTY_FUNCTION__);}
+
+extern Cursorinfo cursor;
+
+typedef struct touch touch;
+typedef struct button button;
+typedef struct pt pt;
+
+
+struct pt
+{
+	float x,y;
+};
+
+struct touch
+{
+	float x,y;
+	int inview;
+	void *tid;
+};
+
+struct button
+{
+	pt min, max, dim;
+};
+
+Memimage *gscreen;
+Screeninfo screen;
+static CGRect devRect;
+static float dx, dy;
+static CGDataProviderRef dataProviderRef;
+static CGImageRef fullScreenImage;
+
+static int isready = 0;
+static int gesturestate;
+static float zoom = 1.0;
+static float pinchdist;
+static float pinchzoom;
+static int magnify;
+
+pt cursoroff;
+
+static uchar cursorbit[16*16*4];
+static CGDataProviderRef cursorProvider;
+static CGImageRef cursorImg;
+
+
+static Cursorinfo	arrow = {
+	.clr = { 0xFF, 0xFF, 0x80, 0x01, 0x80, 0x02, 0x80, 0x0C, 
+		0x80, 0x10, 0x80, 0x10, 0x80, 0x08, 0x80, 0x04, 
+		0x80, 0x02, 0x80, 0x01, 0x80, 0x02, 0x8C, 0x04, 
+		0x92, 0x08, 0x91, 0x10, 0xA0, 0xA0, 0xC0, 0x40, 
+	},
+	.set = { 0x00, 0x00, 0x7F, 0xFE, 0x7F, 0xFC, 0x7F, 0xF0, 
+		0x7F, 0xE0, 0x7F, 0xE0, 0x7F, 0xF0, 0x7F, 0xF8, 
+		0x7F, 0xFC, 0x7F, 0xFE, 0x7F, 0xFC, 0x73, 0xF8, 
+		0x61, 0xF0, 0x60, 0xE0, 0x40, 0x40, 0x00, 0x00, 
+	},
+	.offset = {0,-16},
+};
+
+
+static int mmod = 0;
+
+static button btns[4];
+
+extern UIView *uiview;
+
+
+enum {
+	GestureNone,
+	GesturePinch,
+	GestureToggleKeyboard,
+	GestureChord,
+};
+
+static float origx, origy;
+static float offx, offy;
+
+static int lastmx = 0, lastmy = 0;
+
+static pt PT(float x, float y)
+{
+	pt p = {x,y};
+	return p;
+}
+
+
+touch touches[20]; // 20..just a ridiculous number XXX: should make checks anyways..
+int ntouches = 0;
+
+void sendbuttons(int b, int x, int y);
+
+
+static void screen_invalidate()
+{
+	if(!uiview) {
+		printf("screenload dismissed -- uiview not initialized XXX: can this even happen?\n");
+		return;
+	}
+	
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	[[NSRunLoop mainRunLoop] performSelector: @selector(pleaseRedraw)
+									  target: uiview
+									argument: nil
+									   order:0
+									   modes:[NSArray arrayWithObject: NSRunLoopCommonModes]]; //NSDefaultRunLoopMode]];
+	[pool release];
+}
+
+static float
+touchdist(touch *a, touch *b)
+{
+	return sqrtf((a->x - b->x)*(a->x - b->x) + (a->y - b->y)*(a->y - b->y));
+}
+
+static int
+inbutton(touch *t, int num)
+{
+	if (t->x >= btns[num].min.x && t->x <= btns[num].max.x &&
+		t->y >= btns[num].min.y && t->y <= btns[num].max.y)
+		return 1;
+	
+	return 0;
+}
+
+void
+screen_keystroke(const char *s)
+{
+	Rune r;
+	chartorune(&r, (char*)s);
+	kbdputc(kbdq, r);
+}
+
+static int
+checkmouse()
+{
+	int i,j;
+	int mod = 0;
+	
+
+	for (i = 0; i < ntouches; i++) {
+		touches[i].inview = 1;
+		for (j = 0; j < 3; j++)
+			if (inbutton(&touches[i], (j+1))) {
+				mod |= 1 << j;
+				touches[i].inview = 0;
+			}
+	}
+
+	if (mod != mmod) {
+		mmod = mod;
+		return 1;
+	}
+	mmod = mod;
+	return 0;
+}
+
+void
+screen_touch_began(void* touchid, float x, float y)
+{
+	touch *t;
+	
+	touches[ntouches].tid = touchid;
+	touches[ntouches].x = x;
+	touches[ntouches].y = y;
+	t = &(touches[ntouches]);
+	ntouches++;
+	
+	if(!isready)
+		return;
+	
+	switch (gesturestate) {
+		case GestureNone:
+			if (checkmouse()) {
+				//printf("activating GestureChord\n");
+				gesturestate = GestureChord;
+				//printf("sending chord 1\n");
+				sendbuttons(mmod, lastmx, lastmy);
+				screen_invalidate();
+			} else if (ntouches == 2) {
+				gesturestate = GesturePinch;
+				// orig in SCREEN coordinates --> point under the pinch
+				origx = ((touches[0].x + touches[1].x) * 0.5 - offx) / zoom;
+				origy = ((touches[0].y + touches[1].y) * 0.5 - offy) / zoom;
+				pinchdist = touchdist(&touches[0], &touches[1]);
+				pinchzoom = zoom;
+				magnify = 0;
+			} else if (ntouches == 1) {
+				if (inbutton(&touches[0], 0)) {
+					gesturestate = GestureToggleKeyboard;
+					magnify = 0;
+				} else {
+					//printf("send null A\n");
+					lastmx = (t->x - offx) / zoom;
+					lastmy = (t->y - offy) / zoom;
+					sendbuttons(0, lastmx, lastmy);
+					magnify = 1;
+					screen_invalidate();
+				}
+
+			}
+			break;
+		case GestureChord:
+			checkmouse();
+			if (t->inview) {
+				//NSLog(@"send buttons!\n");
+				lastmx = (t->x - offx) / zoom;
+				lastmy = (t->y - offy) / zoom;
+				magnify = 1;
+				sendbuttons(mmod, lastmx, lastmy);
+			}
+			screen_invalidate();
+			break;
+	}	
+}
+
+void
+screen_touch_moved(void *touchid, float x, float y)
+{
+	int i;
+	touch *t = nil;
+	for (i = 0; i < ntouches; i++)
+		if (touches[i].tid == touchid) {
+			t = &(touches[i]);
+			break;
+		}
+	if (!t) {
+		printf("Warning...moved a touch that never started\n");
+		return;
+	}
+	t->x = x;
+	t->y = y;
+
+	if (!isready)
+		return;
+	
+	//printf("t:%i  %f//%f\n", i, t->x - origx, t->y - origy);
+
+	switch (gesturestate) {
+		case GestureNone:
+			lastmx = (t->x - offx) / zoom;
+			lastmy = (t->y - offy) / zoom;
+			sendbuttons(0, lastmx, lastmy);
+			screen_invalidate();			
+			break;
+		case GesturePinch: {
+			float curpinch = touchdist(&touches[0], &touches[1]);
+			zoom = (curpinch / pinchdist) * pinchzoom;
+			offx = (touches[0].x + touches[1].x) * 0.5 - origx * zoom;
+			offy = (touches[0].y + touches[1].y) * 0.5 - origy * zoom;
+			//zoom = (curpinch / pinchdist) * pinchzoom;
+			screen_invalidate();
+			break;
+		case GestureChord:
+			if (checkmouse() || t->inview) {
+				if (t->inview) {
+					lastmx = (t->x - offx) / zoom;
+					lastmy = (t->y - offy) / zoom;
+					magnify = 1;
+				}
+				sendbuttons(mmod, lastmx, lastmy);
+				screen_invalidate();
+			}
+			break;
+			
+		}
+	}
+}
+
+void
+screen_touch_ended(void *touchid, float x, float y)
+{
+	int i;
+	touch *t = nil;
+	
+	for (i = 0; i < ntouches; i++)
+		if (touches[i].tid == touchid) {
+			t = &(touches[i]);
+			memmove(t, t+1, ((ntouches-- - i) - 1) * sizeof(touch));
+			break;
+		}
+	if (!t) {
+		printf("Warning...ended a touch that never started\n");
+		return;
+	}
+	
+	if (!isready)
+		return;
+
+	switch (gesturestate) {
+		case GestureNone:
+			magnify = 0;
+			screen_invalidate();
+			break;
+		case GesturePinch:
+			if (ntouches < 2)
+				gesturestate = GestureNone;
+			break;
+		case GestureToggleKeyboard:
+			if (inbutton(&touches[0], 0)) {
+				[uiview toggleKey];
+				//printf("toggle keyboard!\n");
+				gesturestate = GestureNone;
+			}
+			break;
+		case GestureChord:
+			if (checkmouse()) {
+				sendbuttons(mmod, lastmx, lastmy);
+				if (!mmod) {
+					//printf("deactivating GestureChord\n");
+					gesturestate = GestureNone;
+					magnify = 0;
+				}
+				screen_invalidate();
+			}
+			break;
+	}
+	//if (ntouches == 0)
+	//	printf("NO TOUCHES\n");
+}
+
+void
+screen_touch_cancelled(void* touchid, float x, float y)
+{
+	// XXX: for now screen_touch_cancelled just means ended...obviously that's not it exactly. but close enough for now
+	screen_touch_ended(touchid, x, y);
+}
+							
+
+void
+screenload(Rectangle r, int depth, uchar *p, Point pt, int step)
+{
+	screen_invalidate();
+}
+
+// PAL - no palette handling.  Don't intend to either.
+void
+getcolor(ulong i, ulong *r, ulong *g, ulong *b)
+{
+	
+	// PAL: Certainly wrong to return a grayscale.
+	*r = i;
+	*g = i;
+	*b = i;
+}
+
+int
+clipwrite(char *snarf)
+{
+	UNIMPL
+	
+	return 0; // failure
+}
+
+uchar*
+attachscreen(Rectangle *r, ulong *chan, int *depth, int *width, int *softscreen, void **X)
+{
+	/* XXX: what is this??*/
+	*r = gscreen->r;
+	*chan = gscreen->chan;
+	*depth = gscreen->depth;
+	*width = gscreen->width;
+	*softscreen = 1;
+	
+	return gscreen->data->bdata;
+}
+void
+flushmemscreen(Rectangle r)
+{
+	// sanity check.  Trips from the initial "terminal"
+    if (r.max.x < r.min.x || r.max.y < r.min.y) return;
+    
+	screenload(r, gscreen->depth, byteaddr(gscreen, ZP), ZP,
+			   gscreen->width*sizeof(ulong));
+}
+
+char*
+clipread(void)
+{
+	UNIMPL
+	
+	return 0;
+}
+
+static void 
+loadcursor(Cursorinfo *c)
+{
+	int i, j;
+	//UNIMPL
+	int col, alpha;
+	
+	lock(&c->lk);
+	
+	cursoroff.x = c->offset.x;
+	cursoroff.y = c->offset.y;
+
+	
+	for (i = 0; i < 16; i++) {
+		for (j = 0; j < 8; j++) {
+			col = ((c->clr[i * 2 + 0] >> (7 - j)) & 1) ? 255 : 0;
+			alpha = ((c->set[i * 2 + 0] >> (7 - j)) & 1) ? 255 : 0;
+			cursorbit[(16*4) * i + (j * 4) + 0] = col;
+			cursorbit[(16*4) * i + (j * 4) + 1] = col;
+			cursorbit[(16*4) * i + (j * 4) + 2] = col;
+			cursorbit[(16*4) * i + (j * 4) + 3] = alpha;
+		}
+		for (j = 0; j < 8; j++) {
+			col = ((c->clr[i * 2 + 1] >> (7 - j)) & 1) ? 255 : 0;
+			alpha = ((c->set[i * 2 + 1] >> (7 - j)) & 1) ? 255 : 0;
+			cursorbit[(16*4) * i + ((j+8) * 4) + 0] = col;
+			cursorbit[(16*4) * i + ((j+8) * 4) + 1] = col;
+			cursorbit[(16*4) * i + ((j+8) * 4) + 2] = col;
+			cursorbit[(16*4) * i + ((j+8) * 4) + 3] = alpha;
+		}		
+	}
+	
+	unlock(&c->lk);
+	
+}
+
+void
+cursorarrow(void)
+{
+	loadcursor(&arrow);
+}
+
+void
+setcursor()
+{
+	loadcursor(&cursor);
+}
+
+void
+mouseset(Point xy)
+{
+	lastmx = xy.x;
+	lastmy = xy.y;
+	screen_invalidate();
+}
+
+void
+setcolor(ulong index, ulong red, ulong green, ulong blue)
+{
+	// assert(0);
+	UNIMPL
+}
+
+#define deg2rad(__ANGLE__) ((__ANGLE__) / 180.0 * M_PI)
+#define rad2deg(__ANGLE__) ((__ANGLE__) / M_PI * 180.0)
+
+
+void dt_updatescreen(UIView *me)
+{
+	CGRect rbounds;
+	CGRect vbounds;
+	UIImage *baseImg;
+	int i;
+	//CGAffineTransform transnorm;
+	rbounds.size.width = dx;
+	rbounds.size.height = dy;
+	rbounds.origin.x = 0;
+	rbounds.origin.y = 0;
+	vbounds = [me bounds];
+	
+	UIGraphicsBeginImageContext(vbounds.size);
+	//UIGraphicsPushContext(context);
+	CGContextRef context = UIGraphicsGetCurrentContext();
+
+	CGAffineTransform transimg = {
+		.a = zoom,
+		.c = 0.0,
+		.tx = floorf(offx),
+		
+		.b = 0.0,
+		.d = -zoom,//-floorf(zoom),
+		.ty = rbounds.size.height * zoom + floorf(offy),//dy
+	};
+	
+	
+	CGContextSaveGState(context);
+	CGContextRotateCTM(context, deg2rad(90.0));
+	CGContextTranslateCTM(context, 0.0,-vbounds.size.width);
+	CGContextConcatCTM(context, transimg);
+	CGContextDrawImage(context, rbounds, fullScreenImage);
+	CGContextDrawImage(context, CGRectMake(lastmx+cursoroff.x,dy - (lastmy-cursoroff.y),16,16), cursorImg);
+	CGContextRestoreGState(context);
+
+
+	CGAffineTransform transbtns = {
+		.a = 0.0,
+		.c = -1.0,
+		.tx = vbounds.size.width,
+		
+		.b = 1.0,
+		.d = 0.0,
+		.ty = 0.0,//dy
+	};
+
+	CGContextConcatCTM(context, transbtns);
+	CGContextMoveToPoint(context, 0, 0);
+
+	for (i = 0; i < nelem(btns); i++) {
+		CGRect r = CGRectMake(btns[i].min.x,btns[i].min.y,btns[i].dim.x, btns[i].dim.y);
+
+		if (mmod & (1 << (i-1)))
+			CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 1.0);
+		else 
+			CGContextSetRGBFillColor(context, 0.0, 0.0, 0.0, 1.0);
+		//CGContextSetRGBFillColor(context, 0.0,0.0,0.0,1.0);
+		CGContextFillRect(context, r);
+		CGContextSetRGBStrokeColor(context, 0.0, 1.0, 0.0, 1.0);
+		CGContextStrokeRect(context, r);
+	}
+	
+	CGContextSetRGBFillColor(context, 0.0,0.0,1.0,1.0);
+	//CGContextFillEllipseInRect(context, CGRectMake((lastmx*zoom+offx)-5,(lastmy*zoom+offy)-5,10,10));
+	//CGContextDrawImage(context, CGRectMake((lastmx*zoom+offx)-40,(lastmy*zoom+offy)-40,80,80), cursorImg);
+	
+	CGContextFlush(context);
+
+	baseImg = [UIGraphicsGetImageFromCurrentImageContext() retain];
+	//UIGraphicsPopContext();
+	UIGraphicsEndImageContext();
+	context = UIGraphicsGetCurrentContext();
+
+	//CGContext
+	//CGContextTranslateCTM(context, 0.0,-vbounds.size.width);
+	CGContextSaveGState(context);
+	CGContextScaleCTM(context, 1.0, -1.0);
+	CGContextTranslateCTM(context, 0, -vbounds.size.height);
+	CGContextDrawImage(context, vbounds, [baseImg CGImage]);	
+	CGContextRestoreGState(context);
+
+	if (magnify) {
+		float sx = lastmx * zoom + offx;
+		float sy = lastmy * zoom + offy;
+		CGRect elr = CGRectMake(vbounds.size.width-sy+50, sx-50, 100, 100);
+		CGContextFillEllipseInRect(context, elr);
+		CGContextAddEllipseInRect(context, CGRectMake(vbounds.size.width-sy+55, sx-45, 90, 90));
+		CGContextClip(context);
+		CGContextScaleCTM(context, 1.0, -1.0);
+		CGContextTranslateCTM(context, 100, -vbounds.size.height);
+		CGContextDrawImage(context, CGRectMake(vbounds.origin.x-(vbounds.size.width-sy), vbounds.origin.y-(vbounds.size.height-sx), vbounds.size.width*2.0, vbounds.size.height*2.0), [baseImg CGImage]);
+	}
+	
+	CGContextFlush(context);
+	
+	[baseImg release];
+}
+
+void
+btnsinit()
+{
+	int i;
+	CGRect b = [uiview bounds];
+	const float bsize = 80;
+
+	for (i = 0; i < nelem(btns); i++) {
+		btns[i].min.x = b.size.height - bsize; 
+		btns[i].max.x = b.size.height;
+		btns[i].min.y = bsize * i;
+		btns[i].max.y = bsize * (i + 1);
+		btns[i].dim.x = bsize;
+		btns[i].dim.y = bsize;
+	}
+}
+
+void screeninit(void)
+{
+	memimageinit();
+	screen.depth = 32;
+
+	devRect = CGRectMake(0,0,1024,768); // XXX: we have to pick something... maybe this should be a preference?
+	
+	dx = devRect.size.width;
+	dy = devRect.size.height;
+
+	gscreen = allocmemimage(Rect(0,0,dx,dy), XBGR32);
+	
+	dataProviderRef = CGDataProviderCreateWithData(0, gscreen->data->bdata,
+												   dx * dy * 4, 0);
+	fullScreenImage = CGImageCreate(dx, dy, 8, 32, dx * 4,
+									CGColorSpaceCreateDeviceRGB(),
+									kCGImageAlphaNoneSkipLast,
+									dataProviderRef, 0, 0, kCGRenderingIntentDefault);
+	cursorProvider = CGDataProviderCreateWithData(0, cursorbit, 16 * 16 * 4, 0);
+	cursorImg = CGImageCreate(16, 16, 8, 32, 16 * 4,
+									CGColorSpaceCreateDeviceRGB(),
+									kCGImageAlphaPremultipliedLast,
+									cursorProvider, 0, 0, kCGRenderingIntentDefault);
+	
+
+	loadcursor(&arrow);
+	btnsinit();
+	terminit();
+	isready = 1;
+}
+
+
---
+++ kern/dev-ios.c
@@ -0,0 +1,271 @@
+#include	"u.h"
+#include	"lib.h"
+#include	"dat.h"
+#include	"fns.h"
+#include	"error.h"
+
+#include	"draw.h"
+#include	"memdraw.h"
+#include	"screen.h"
+
+typedef struct Accelinfo Accelinfo;
+typedef struct Locationinfo Locationinfo;
+
+struct Accelinfo
+{
+	Lock lk;
+	int open;
+	int changed;
+	float x,y,z;
+	Rendez r;
+};
+
+struct Locationinfo
+{
+	Lock lk;
+	Rendez r;
+	int open, changed, logging, failed;
+	float x, y, altitude, haccur, vaccur;	
+};
+
+Accelinfo accel;
+Locationinfo location;
+
+enum{
+	Qdir,
+	Qaccel,
+	Qlocation,
+};
+
+Dirtab iosdir[]={
+	".",		{Qdir, 0, QTDIR},	0,	DMDIR|0555,	
+	"accel",	{Qaccel},	0,			0444,
+	"location",	{Qlocation},0,			0444,
+	
+};
+
+#define	NIOS	(sizeof(iosdir)/sizeof(Dirtab))
+#define IOSCHAR 'z'
+
+
+void loglocation(); // startup.m
+
+static int accelchanged(void *a);
+static int locationchanged(void *a);
+
+static Chan*
+iosattach(char *spec)
+{
+	return devattach(IOSCHAR, spec);
+}
+
+static Walkqid*
+ioswalk(Chan *c, Chan *nc, char **name, int nname)
+{
+	return devwalk(c, nc, name, nname, iosdir, NIOS, devgen);
+}
+
+static int
+iosstat(Chan *c, uchar *db, int n)
+{
+	return devstat(c, db, n, iosdir, NIOS, devgen);
+}
+
+static Chan*
+iosopen(Chan *c, int omode)
+{
+	switch((long)c->qid.path){
+	case Qdir:
+		if(omode != OREAD)
+			error(Eperm);
+		break;
+	case Qaccel:
+			if (omode != OREAD)
+				error(Eperm);
+			lock(&accel.lk);
+			if (accel.open) {
+				unlock(&accel.lk);
+				error(Einuse);
+			}
+			accel.open = 1;
+			unlock(&accel.lk);
+			break;
+	case Qlocation:
+			if (omode != OREAD)
+				error(Eperm);
+			lock(&location.lk);
+			if (location.open) {
+				unlock(&location.lk);
+				error(Einuse);
+			}
+			if (!location.logging) {
+				loglocation();
+				location.logging = 1;
+			} else {
+				// updates coming so rarely
+				// just send the last one again when reopening the device
+				location.changed = 1;
+			}
+			location.open = 1;
+			unlock(&location.lk);
+			break;
+	}
+	
+	c->mode = openmode(omode);
+	c->flag |= COPEN;
+	c->offset = 0;
+	return c;
+}
+
+void
+iosclose(Chan *c)
+{
+	if(!(c->flag&COPEN))
+		return;
+
+	switch((long)c->qid.path) {
+		case Qaccel:
+			lock(&accel.lk);
+			accel.open = 0;
+			unlock(&accel.lk);
+			break;
+		case Qlocation:
+			lock(&location.lk);
+			location.open = 0;
+			unlock(&location.lk);
+			break;
+	}
+}
+
+
+long
+iosread(Chan *c, void *va, long n, vlong offset)
+{
+	char buf[5*12+1];
+	// XXX: why not write directly into va?
+	uchar *p;
+	int len;
+	
+	p = va;
+	switch((long)c->qid.path){
+		case Qdir:
+			return devdirread(c, va, n, iosdir, NIOS, devgen);
+		case Qaccel:
+			while (!accelchanged(0))
+				sleep(&accel.r, accelchanged, 0);
+			lock(&accel.lk);
+			sprint(buf, "a%11f %11f %11f", accel.x, accel.y, accel.z);
+			accel.changed = 0;
+			unlock(&accel.lk);
+			if (n > nelem(buf))
+				n = nelem(buf);
+			memmove(va, buf, n);
+			return n;
+		break;
+		case Qlocation:
+			while(!locationchanged(0))
+				sleep(&location.r, locationchanged, 0);
+			lock(&location.lk);
+			if (location.failed) {
+				error("cannot read from location service");
+				unlock(&location.lk);
+				return -1;
+			}
+			len = sprint(buf, "%11f %11f %11f %11f %11f\n", location.x, location.y, location.altitude, location.haccur, location.vaccur);
+			location.changed = 0;
+			unlock(&location.lk);
+			if (n > len)
+				n = len;
+			memmove(va, buf, n);
+			return n;
+	}
+	return 0;
+}
+
+long
+ioswrite(Chan *c, void *va, long n, vlong offset)
+{
+	char *p;
+
+	USED(offset);
+
+	p = va;
+	switch((long)c->qid.path){
+	case Qdir:
+		error(Eisdir);
+	case Qaccel:
+		panic("shouldn't be able to write on Qaccel");
+		break;
+	case Qlocation:
+		panic("shouldn't be able to write on Qlocation");
+		break;			
+	}
+
+	error(Egreg);
+	return -1;
+}
+
+
+static int
+accelchanged(void *a)
+{
+	USED(a);
+	
+	return accel.changed;
+}
+
+static int
+locationchanged(void *a)
+{
+	USED(a);
+	return location.changed;
+}
+
+void
+sendaccel(float x, float y, float z)
+{
+	lock(&accel.lk);
+	accel.x = x;
+	accel.y = y;
+	accel.z = z;
+	accel.changed = 1;
+	unlock(&accel.lk);
+	wakeup(&accel.r);
+}
+
+void
+sendlocation(float x, float y, float altitude, float haccuracy, float vaccuracy, int failed) // Coordinates in WGS84
+{
+	lock(&location.lk);
+	location.x = x;
+	location.y = y;
+	location.altitude = altitude;
+	location.haccur = haccuracy;
+	location.vaccur = vaccuracy;
+	location.failed = failed;
+	location.changed = 1;
+	unlock(&location.lk);
+	wakeup(&location.r);
+}
+
+
+Dev iosdevtab = {
+	IOSCHAR,
+	"ios",
+
+	devreset,
+	devinit,
+	devshutdown,
+	iosattach,
+	ioswalk,
+	iosstat,
+	iosopen,
+	devcreate,
+	iosclose,
+	iosread,
+	devbread,
+	ioswrite,
+	devbwrite,
+	devremove,
+	devwstat,
+};
