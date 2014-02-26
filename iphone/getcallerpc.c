4a0bc6a39622a81d963146c61491e88a78856763
Jeff Sickel <jas@corpus-callosum.com>
2014-02-26 14:27:05 -0600

    new Images.xcassets

--- iphone/getcallerpc.c
+++
@@ -1,9 +0,0 @@
-#include "u.h"
-#include "libc.h"
-
-// XXX: i don't know if that is even remotely right...it hasn't yet caused any obvious troubles though
-uintptr
-getcallerpc(void *a)
-{
-	return ((uintptr*)a)[-1];
-}
